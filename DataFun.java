a47bde6e176f21f48f2516262d62af4ba7ad0c30
NotaRubow <rubown@wlhs.wlwv.k12.or.us>
2024-11-05 23:07:42 +0000

    start of code

--- DataFun.java
+++ DataFun.java
@@ -1,12 +1,22 @@
 // any imports here
-
+import java.util.Scanner;
 /**
-     * Description goes here
-     * @author [Student Name]
-     * @version [Current Date]
+     * Data Fun Project
+     * @author Nathan Rubow
+     * @version 11/5/2024
      */
 public class DataFun{
     public static void main(String[] args){
-        
+        System.out.println("What is your favorite integer? ");
+        Scanner fun = new Scanner(System.in);
+        int userFavInt =  fun.nextInt();
+        if (userFavInt>=0)
+            {
+            System.out.println(userFavInt + " is a positive number");
+            }
+        else
+        {
+        System.out.println(userFavInt + " is a negative number");
+        }
     }
 }
